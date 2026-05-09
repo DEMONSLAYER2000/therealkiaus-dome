0860ea3e34f5b5e73f36b0fabfa9c96107ee803f
DEMONSLAYER2000 <therealkiau@gmail.com>
2026-05-09 18:41:24 -0400

    Update games.js

--- games.js
+++ games.js
@@ -1,69 +1,54 @@
+let category = "all";
+
 const games = [
+  // FPS / ACTION
+  { name: "Krunker", url: "https://krunker.io", cat: "fps" },
+  { name: "1v1.LOL", url: "https://1v1.lol", cat: "fps" },
+  { name: "Shell Shockers", url: "https://shellshock.io", cat: "fps" },
+  { name: "Venge.io", url: "https://venge.io", cat: "fps" },
 
-{
-name: "Krunker",
-url: "https://krunker.io",
-cat: "fps"
-},
+  // IO GAMES
+  { name: "Agar.io", url: "https://agar.io", cat: "io" },
+  { name: "Diep.io", url: "https://diep.io", cat: "io" },
+  { name: "Slither.io", url: "https://slither.io", cat: "io" },
+  { name: "Paper.io 2", url: "https://paper-io.com", cat: "io" },
 
-{
-name: "1v1.LOL",
-url: "https://1v1.lol",
-cat: "shooter"
-},
+  // SANDBOX / BUILDING
+  { name: "Bloxd.io", url: "https://bloxd.io", cat: "sandbox" },
+  { name: "Minecraft Classic", url: "https://classic.minecraft.net", cat: "sandbox" },
+  { name: "Roblox", url: "https://www.roblox.com", cat: "sandbox" },
 
-{
-name: "Smash Karts",
-url: "https://smashkarts.io",
-cat: "multiplayer"
-},
+  // DRIVING
+  { name: "Slow Roads", url: "https://slowroads.io", cat: "driving" },
+  { name: "Madalin Stunt Cars 2", url: "https://www.madalinstuntcars2.com", cat: "driving" },
 
-{
-name: "Agar.io",
-url: "https://agar.io",
-cat: "multiplayer"
-},
+  // PUZZLE
+  { name: "2048", url: "https://play2048.co", cat: "puzzle" },
+  { name: "Tetris", url: "https://tetris.com/play-tetris", cat: "puzzle" },
+  { name: "Cut the Rope", url: "https://cuttherope.net", cat: "puzzle" },
 
-{
-name: "Diep.io",
-url: "https://diep.io",
-cat: "action"
-},
+  // BOARD / STRATEGY
+  { name: "Chess.com", url: "https://www.chess.com/play/online", cat: "board" },
+  { name: "Checkers", url: "https://www.mathsisfun.com/games/checkers.html", cat: "board" },
 
-{
-name: "Shell Shockers",
-url: "https://shellshock.io",
-cat: "fps"
-},
+  // RACING / SPORTS
+  { name: "Smash Karts", url: "https://smashkarts.io", cat: "racing" },
+  { name: "Moto X3M", url: "https://www.crazygames.com/game/moto-x3m", cat: "racing" },
 
-{
-name: "Bloxd.io",
-url: "https://bloxd.io",
-cat: "sandbox"
-},
+  // CLASSICS / RETRO
+  { name: "Pac-Man", url: "https://www.google.com/logos/2010/pacman10-i.html", cat: "retro" },
+  { name: "Snake", url: "https://www.google.com/fbx?fbx=snake_arcade", cat: "retro" },
 
-{
-name: "Slow Roads",
-url: "https://slowroads.io",
-cat: "driving"
-},
+  // SURVIVAL / ADVENTURE
+  { name: "Terraria (info/play links vary)", url: "https://terraria.org", cat: "adventure" },
+  { name: "Run 3", url: "https://run3.io", cat: "adventure" },
 
-{
-name: "2048",
-url: "https://play2048.co",
-cat: "puzzle"
-},
+  // ADD MORE EXPANDABLE SLOTS
+  { name: "Geometry Dash Lite", url: "https://geometrydashlite.co", cat: "rhythm" },
+  { name: "Stickman Hook", url: "https://stickmanhookgame.com", cat: "arcade" },
 
-{
-name: "Chess",
-url: "https://www.chess.com/play",
-cat: "strategy"
-},
-
-{
-name: "Tetris",
-url: "https://tetris.com/play-tetris",
-cat: "puzzle"
-}
-
+  // EXTRA IO GAMES
+  { name: "WormsZone.io", url: "https://wormate.io", cat: "io" },
+  { name: "Hole.io", url: "https://hole-io.com", cat: "io" },
+  { name: "ZombsRoyale", url: "https://zombsroyale.io", cat: "fps" }
 ];
